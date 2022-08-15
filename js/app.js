43aa76773655e90a5b0125ba57a56afd2765cb4a
hyun_jin98 <105402290+wowjdql98@users.noreply.github.com>
2022-08-15 22:11:29 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -75,22 +75,15 @@ $(function () {
     }
 })
 
-// 토글메뉴의 메뉴 슬라이드 애니메이션
-$(function () {
-    $('#toggle-gnb > li').hover(function () {
-        $(this).find('.sub').stop().slideDown();
-    }, function () {
-        $(this).find('.sub').stop().slideUp();
-    });
-});
-
 // 모바일 버전의 토글메뉴의 메뉴 슬라이드 애니메이션
 $(function () {
-    $('#toggle-gnb > li > .gnb-plus').click(function () {
-        $(this).find('.sub').stop().slideDown();
+    $('#toggle-gnb > li ').hover(function(){
+        $(this).find('.sub').toggle(function(){
+            $(this).find('.sub').stop().slideDown();
+            $(this).find('.sub').stop().slideUp();
+        });
     });
 });
-
 
 // 검색바 뿅 
 $(function () {
@@ -185,6 +178,156 @@ $(function () {
 
 })
 
+// 대관안내 모달 
+$(function(){
+    $('#rental #slider label .click-modal .rental-detail').click(function(){
+        $('.rental-modal').addClass('on')
+    });
+    $('#rental .rental-modal #close-btn').click(function(){
+        $('#rental .rental-modal').removeClass('on')
+    });
+});
+
+// 디테일 찜하기 하트 변경
+$(function () {
+    $(".ht1").click(
+        function () {
+            $(this).attr("src", "../images/heart1.png");
+        },
+        function () {
+            $(this).attr("src", "../images/heart2.png");
+        });
+});
+
+
+// 달력 파트
+$(function () {
+    function c() {
+        p();
+        var e = h();
+        var r = 0;
+        var u = false;
+        l.empty();
+        while (!u) {
+            if (s[r] == e[0].weekday) {
+                u = true
+            } else {
+                l.append('<div class="blank"></div>');
+                r++
+            }
+        }
+        for (var c = 0; c < 42 - r; c++) {
+            if (c >= e.length) {
+                l.append('<div class="blank"></div>')
+            } else {
+                var v = e[c].day;
+                var m = g(new Date(t, n - 1, v)) ? '<div class="today">' : "<div>";
+                l.append(m + "" + v + "</div>")
+            }
+        }
+        var y = o[n - 1];
+        a.css("background-color", y).find("h1").text(i[n - 1] + " " + t);
+        f.find("div").css("color", y);
+        l.find(".today").css("background-color", y);
+        d()
+    }
+
+    function h() {
+        var e = [];
+        for (var r = 1; r < v(t, n) + 1; r++) {
+            e.push({
+                day: r,
+                weekday: s[m(t, n, r)]
+            })
+        }
+        return e
+    }
+
+    function p() {
+        f.empty();
+        for (var e = 0; e < 7; e++) {
+            f.append("<div>" + s[e].substring(0, 3) + "</div>")
+        }
+    }
+
+    function d() {
+        var t;
+        var n = $("#calendar").css("width", e + "px");
+        n.find(t = "#calendar_weekdays, #calendar_content").css("width", e + "px").find("div").css({
+            width: e / 7 + "px",
+            height: e / 7 + "px",
+            "line-height": e / 7 + "px"
+        });
+        n.find("#calendar_header").css({
+            height: e * (1 / 7) + "px"
+        }).find('i[class^="icon-chevron"]').css("line-height", e * (1 / 7) + "px")
+    }
+
+    function v(e, t) {
+        return (new Date(e, t, 0)).getDate()
+    }
+
+    function m(e, t, n) {
+        return (new Date(e, t - 1, n)).getDay()
+    }
+
+    function g(e) {
+        return y(new Date) == y(e)
+    }
+
+    function y(e) {
+        return e.getFullYear() + "/" + (e.getMonth() + 1) + "/" + e.getDate()
+    }
+
+    function b() {
+        var e = new Date;
+        t = e.getFullYear();
+        n = e.getMonth() + 1
+    }
+    var e = 370;
+    var t = 2013;
+    var n = 9;
+    var r = [];
+    var i = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+    var s = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var o = ["#16a085", "#1abc9c", "#c0392b", "#27ae60", "#FF6860", "#f39c12", "#f1c40f", "#e67e22", "#2ecc71", "#e74c3c", "#d35400", "#2c3e50"];
+    var u = $("#calendar");
+    var a = u.find("#calendar_header");
+    var f = u.find("#calendar_weekdays");
+    var l = u.find("#calendar_content");
+    b();
+    c();
+    a.find('i[class^="icon-chevron"]').on("click", function () {
+        var e = $(this);
+        var r = function (e) {
+            n = e == "next" ? n + 1 : n - 1;
+            if (n < 1) {
+                n = 12;
+                t--
+            } else if (n > 12) {
+                n = 1;
+                t++
+            }
+            c()
+        };
+        if (e.attr("class").indexOf("left") != -1) {
+            r("previous")
+        } else {
+            r("next")
+        }
+    })
+})
+
+
+
+$(function () {
+    $('.a2').click(function () {
+        $('#calendar').addClass('on');
+    });
+    $('.exit').click(function () {
+        $('#calendar').removeClass('on');
+    });
+});
 
 
 // footer 
@@ -192,33 +335,33 @@ $(function () {
     //instagram hover
     $("#insta").hover(
         function () {
-            $(this).attr("src", "images/instagram_2.png");
+            $(this).attr("src", "../images/instagram_2.png");
         },
         function () {
-            $(this).attr("src", "images/instagram.png");
+            $(this).attr("src", "../images/instagram.png");
         });
     //youtube hover
     $("#youtube").hover(
         function () {
-            $(this).attr("src", "images/youtube_2.png");
+            $(this).attr("src", "../images/youtube_2.png");
         },
         function () {
-            $(this).attr("src", "images/youtube.png");
+            $(this).attr("src", "../images/youtube.png");
         });
     //kakao hover
     $("#kakaotalk").hover(
         function () {
-            $(this).attr("src", "images/kakao_2.png");
+            $(this).attr("src", "../images/kakao_2.png");
         },
         function () {
-            $(this).attr("src", "images/kakao.png");
+            $(this).attr("src", "../images/kakao.png");
         });
     //naver hover
     $("#naver").hover(
         function () {
-            $(this).attr("src", "images/navrer_1.png");
+            $(this).attr("src", "../images/navrer_1.png");
         },
         function () {
-            $(this).attr("src", "images/navrer.png");
+            $(this).attr("src", "../images/navrer.png");
         });
 });
